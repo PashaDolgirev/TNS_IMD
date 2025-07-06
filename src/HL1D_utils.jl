7ce38a3bd94aed7a13eaf5b7d148eb07cd1f88ff
PDolgirevPhysics <p_dolgirev@g.harvard.edu>
2025-07-06 16:48:01 -0400

    current up to date

--- src/HL1D_utils.jl
+++ src/HL1D_utils.jl
@@ -1,7 +1,7 @@
 # generates translationally invariant Hamiltonian terms
 # we consider one, two, and three body Pauli terms 
 # bulk of each generated term commutes with the string X1*X2*...*XN
-function generate_custom_Hamiltonian(hl::HamiltonianLearner, coeff_list::Union{Vector{Float64}, Nothing}=nothing, range::Union{Int, Nothing}=2)
+function generate_custom_Hamiltonian(hl::HamiltonianLearner, coeff_list::Union{Vector{Float64}, Nothing}=nothing)
     N = length(hl.sites)
     H = AutoMPO()
 
@@ -17,45 +17,70 @@ function generate_custom_Hamiltonian(hl::HamiltonianLearner, coeff_list::Union{V
     ind_op += 1
 
     #two-body Pauli terms
-    for l in 1:range
-        for j in 1:(N - l)
-            H .+= 4.0 * coeff_list[ind_op], "Sx", j, "Sx", j + l
-            H .+= 4.0 * coeff_list[ind_op + 1], "Sy", j, "Sy", j + l
-            H .+= 4.0 * coeff_list[ind_op + 2], "Sz", j, "Sz", j + l
-            H .+= 4.0 * coeff_list[ind_op + 3], "Sy", j, "Sz", j + l
-            H .+= 4.0 * coeff_list[ind_op + 4], "Sz", j, "Sy", j + l
-        end
-        ind_op += 5
+    for j in 1:(N - 1)
+        H .+= 4.0 * coeff_list[ind_op], "Sx", j, "Sx", j + 1
+        H .+= 4.0 * coeff_list[ind_op + 1], "Sy", j, "Sy", j + 1
+        H .+= 4.0 * coeff_list[ind_op + 2], "Sz", j, "Sz", j + 1
+        H .+= 4.0 * coeff_list[ind_op + 3], "Sy", j, "Sz", j + 1
+        H .+= 4.0 * coeff_list[ind_op + 4], "Sz", j, "Sy", j + 1
     end
+    ind_op += 5
     
     #three-body Pauli terms
-    for l1 in 1:(range - 1)
-        for l2 in (l1 + 1):range
-            for j in 1:(N - l2)
-                H .+= 8.0 * coeff_list[ind_op], "Sx", j, "Sx", j + l1, "Sx", j + l2
-
-                H .+= 8.0 * coeff_list[ind_op + 1], "Sy", j, "Sx", j + l1, "Sy", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 2], "Sz", j, "Sx", j + l1, "Sz", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 3], "Sy", j, "Sx", j + l1, "Sz", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 4], "Sz", j, "Sx", j + l1, "Sy", j + l2
-
-                H .+= 8.0 * coeff_list[ind_op + 5], "Sy", j, "Sy", j + l1, "Sx", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 6], "Sz", j, "Sz", j + l1, "Sx", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 7], "Sy", j, "Sz", j + l1, "Sx", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 8], "Sz", j, "Sy", j + l1, "Sx", j + l2
-
-                H .+= 8.0 * coeff_list[ind_op + 9], "Sx", j, "Sy", j + l1, "Sy", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 10], "Sx", j, "Sz", j + l1, "Sz", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 11], "Sx", j, "Sy", j + l1, "Sz", j + l2
-                H .+= 8.0 * coeff_list[ind_op + 12], "Sx", j, "Sz", j + l1, "Sy", j + l2
-            end
-            ind_op += 13
-        end
+    #bulk terms
+    for j in 1:(N - 2)
+        H .+= 8.0 * coeff_list[ind_op], "Sx", j, "Sx", j + 1, "Sx", j + 2
+        H .+= 8.0 * coeff_list[ind_op + 1], "Sy", j, "Sx", j + 1, "Sy", j + 2
+        H .+= 8.0 * coeff_list[ind_op + 2], "Sz", j, "Sx", j + 1, "Sz", j + 2
+        H .+= 8.0 * coeff_list[ind_op + 3], "Sy", j, "Sx", j + 1, "Sz", j + 2
+        H .+= 8.0 * coeff_list[ind_op + 4], "Sz", j, "Sx", j + 1, "Sy", j + 2
     end
+    #corresponding boundary terms
+    # H .+= 4.0 * coeff_list[ind_op], "Sx", 1, "Sx", 2
+    # H .+= 4.0 * coeff_list[ind_op], "Sx", N - 1, "Sx", N
+    # H .+= 4.0 * coeff_list[ind_op + 1], "Sx", 1, "Sy", 2
+    # H .+= 4.0 * coeff_list[ind_op + 1], "Sy", N - 1, "Sx", N
+    # H .+= 4.0 * coeff_list[ind_op + 2], "Sx", 1, "Sz", 2
+    # H .+= 4.0 * coeff_list[ind_op + 2], "Sz", N - 1, "Sx", N
+    # H .+= 4.0 * coeff_list[ind_op + 3], "Sx", 1, "Sz", 2
+    # H .+= 4.0 * coeff_list[ind_op + 3], "Sy", N - 1, "Sx", N
+    # H .+= 4.0 * coeff_list[ind_op + 4], "Sx", 1, "Sy", 2
+    # H .+= 4.0 * coeff_list[ind_op + 4], "Sz", N - 1, "Sx", N
+    ind_op += 5
+
+    #boundary terms - single Pauli
+    H .+= 2.0 * coeff_list[ind_op], "Sx", 1
+    H .+= 2.0 * coeff_list[ind_op + 1], "Sx", N
+    H .+= 2.0 * coeff_list[ind_op + 2], "Sy", 1
+    H .+= 2.0 * coeff_list[ind_op + 3], "Sy", N
+    H .+= 2.0 * coeff_list[ind_op + 4], "Sz", 1
+    H .+= 2.0 * coeff_list[ind_op + 5], "Sz", N
+    ind_op += 6
+
+    #boundary terms - two Paulis
+    H .+= 4.0 * coeff_list[ind_op], "Sx", 1, "Sx", 2
+    H .+= 4.0 * coeff_list[ind_op + 1], "Sx", 1, "Sy", 2
+    H .+= 4.0 * coeff_list[ind_op + 2], "Sx", 1, "Sz", 2
+    H .+= 4.0 * coeff_list[ind_op + 3], "Sy", 1, "Sx", 2
+    H .+= 4.0 * coeff_list[ind_op + 4], "Sy", 1, "Sy", 2
+    H .+= 4.0 * coeff_list[ind_op + 5], "Sy", 1, "Sz", 2
+    H .+= 4.0 * coeff_list[ind_op + 6], "Sz", 1, "Sx", 2
+    H .+= 4.0 * coeff_list[ind_op + 7], "Sz", 1, "Sy", 2
+    H .+= 4.0 * coeff_list[ind_op + 8], "Sz", 1, "Sz", 2
+    ind_op += 9
+
+    H .+= 4.0 * coeff_list[ind_op], "Sx", N - 1, "Sx", N
+    H .+= 4.0 * coeff_list[ind_op + 1], "Sx", N - 1, "Sy", N
+    H .+= 4.0 * coeff_list[ind_op + 2], "Sx", N - 1, "Sz", N
+    H .+= 4.0 * coeff_list[ind_op + 3], "Sy", N - 1, "Sx", N
+    H .+= 4.0 * coeff_list[ind_op + 4], "Sy", N - 1, "Sy", N
+    H .+= 4.0 * coeff_list[ind_op + 5], "Sy", N - 1, "Sz", N
+    H .+= 4.0 * coeff_list[ind_op + 6], "Sz", N - 1, "Sx", N
+    H .+= 4.0 * coeff_list[ind_op + 7], "Sz", N - 1, "Sy", N
+    H .+= 4.0 * coeff_list[ind_op + 8], "Sz", N - 1, "Sz", N
 
     return H
 end
-
 
 function generate_Hamiltonian(hl::HamiltonianLearner, coeff_list::Union{Vector{Float64}, Nothing}=nothing, flag_diag=true)
     if isnothing(coeff_list)
@@ -105,21 +130,24 @@ function generate_correlation_matrix(hl::HamiltonianLearner)
 
     G_mat = 0.5 * (G_mat + G_mat')
     hl.G_mat = copy(G_mat)
+    hl.exp_vals = copy(expectation_vals)
     return G_mat
 end
 
 
-function find_optimal_eigenvec(V::Vector{Vector{Float64}})
+function find_optimal_eigenvec(V::Vector{Vector{Float64}}, exp_vals::Vector{Float64})
     d = length(V)
 
     custom_weights = [0.0; 
                          ones(Float64, 5);#2-Pauli nn
-                         4.0 * ones(Float64, 5); #2-Pauli nnn
                          4.0 * ones(Float64, 5); #3-Pauli
-                         8.0 * ones(Float64, 8)  #3-Pauli skewed
+                         10.0 * ones(Float64, 6); #boundary: single Pauli
+                         1000.0 * ones(Float64, 18); #boundary: two Paulis
                          ] 
-    custom_weights[4] = 0.5  # prefer ZZ
-    custom_weights[14] = 1.5  # prefer ZXZ
+    custom_weights[4] = -100.5  # prefer ZZ
+    custom_weights[9] = -1000.5  # prefer ZXZ
+    custom_weights[20] = -1000.5 # prefer ZXZ
+    custom_weights[33] = -1000.5 # prefer ZXZ
 
     function get_vec(x)
         v = zero(V[1])
@@ -140,6 +168,14 @@ function find_optimal_eigenvec(V::Vector{Vector{Float64}})
     result = optimize(cost_vec, x0, NelderMead())
     x_opt = Optim.minimizer(result)
 
+    function cost_vec_2(x)
+        v = get_vec(x)
+        return sum((v .^ 2) .* custom_weights) + 1000.0 * sum(exp_vals .* v)
+    end
+
+    result = optimize(cost_vec_2, x_opt, NelderMead())
+    x_opt = Optim.minimizer(result)
+
     v_opt = get_vec(x_opt)
     return v_opt
 end
@@ -154,7 +190,7 @@ function LearnHamiltonian_corr_mat(hl::HamiltonianLearner)
     V = Vector{Vector{Float64}}()
 
     for n in 1:length(eigvals)
-        if abs(eigvals[n] - λ_min) < 1e-12
+        if abs(eigvals[n] - λ_min) < 1e-10
             push!(V, eigvecs[:, n])
         end
     end
@@ -162,7 +198,7 @@ function LearnHamiltonian_corr_mat(hl::HamiltonianLearner)
     if length(V) == 1
         v_min = V[1] / norm(V[1])
     else
-        v_min = find_optimal_eigenvec(V)
+        v_min = find_optimal_eigenvec(V, hl.exp_vals)
     end
 
     H = generate_custom_Hamiltonian(hl, v_min)
--- src/HL_module.jl
+++ src/HL_module.jl
@@ -12,10 +12,10 @@ mutable struct HamiltonianLearner
     psi::MPS                    # state vector
     coeff_list::Vector{Float64} # learned coefficients
     op_list::Vector{AutoMPO}    # Hamiltonian ansatz
-    range::Int                  # encodes locality of the Hamiltonian
 
     method::Symbol              # :corr_mat or :IQA
     G_mat::Matrix{Float64}      # correlation matrix
+    exp_vals::Vector{Float64}   # expectation_vals wrt to psi
 
     H::MPO                      # learned Hamiltonian
     ψ_GS::MPS                   # ground state
@@ -29,7 +29,8 @@ end
 # generates translationally invariant Hamiltonian terms
 # we consider one, two, and three body Pauli terms 
 # bulk of each generated term commutes with the string X1*X2*...*XN
-function generate_custom_op_list(sites::IndexSet, range::Int)
+# Note that the boundary terms can explicitly break this symmetry
+function generate_custom_op_list(sites::IndexSet)
     N = length(sites)
 
     op_list = AutoMPO[]
@@ -42,91 +43,117 @@ function generate_custom_op_list(sites::IndexSet, range::Int)
     push!(op_list, X_term)
 
     #two-body Pauli terms
-    for l in 1:range
-        op_XX = AutoMPO()
-        op_YY = AutoMPO()
-        op_ZZ = AutoMPO()
-        op_YZ = AutoMPO()
-        op_ZY = AutoMPO()
+    op_XX = AutoMPO()
+    op_YY = AutoMPO()
+    op_ZZ = AutoMPO()
+    op_YZ = AutoMPO()
+    op_ZY = AutoMPO()
 
-        for j in 1:(N - l)
-            op_XX .+= 4.0, "Sx", j, "Sx", j + l
-            op_YY .+= 4.0, "Sy", j, "Sy", j + l
-            op_ZZ .+= 4.0, "Sz", j, "Sz", j + l
-            op_YZ .+= 4.0, "Sy", j, "Sz", j + l
-            op_ZY .+= 4.0, "Sz", j, "Sy", j + l
-        end
-
-        push!(op_list, op_XX)
-        push!(op_list, op_YY)
-        push!(op_list, op_ZZ)
-        push!(op_list, op_YZ)
-        push!(op_list, op_ZY)
+    for j in 1:(N - 1)
+        op_XX .+= 4.0, "Sx", j, "Sx", j + 1
+        op_YY .+= 4.0, "Sy", j, "Sy", j + 1
+        op_ZZ .+= 4.0, "Sz", j, "Sz", j + 1
+        op_YZ .+= 4.0, "Sy", j, "Sz", j + 1
+        op_ZY .+= 4.0, "Sz", j, "Sy", j + 1
     end
-    
+
+    push!(op_list, op_XX)
+    push!(op_list, op_YY)
+    push!(op_list, op_ZZ)
+    push!(op_list, op_YZ)
+    push!(op_list, op_ZY)
+
     #three-body Pauli terms
-    for l1 in 1:(range - 1)
-        for l2 in (l1 + 1):range
+    op_XXX = AutoMPO()
+    op_YXY = AutoMPO()
+    op_ZXZ = AutoMPO()
+    op_YXZ = AutoMPO()
+    op_ZXY = AutoMPO()
 
-            op_XXX = AutoMPO()
-
-            op_YXY = AutoMPO()
-            op_ZXZ = AutoMPO()
-            op_YXZ = AutoMPO()
-            op_ZXY = AutoMPO()
-
-            op_YYX = AutoMPO()
-            op_ZZX = AutoMPO()
-            op_YZX = AutoMPO()
-            op_ZYX = AutoMPO()
-
-            op_XYY = AutoMPO()
-            op_XZZ = AutoMPO()
-            op_XYZ = AutoMPO()
-            op_XZY = AutoMPO()
-
-            for j in 1:(N - l2)
-                op_XXX .+= 8.0, "Sx", j, "Sx", j + l1, "Sx", j + l2
-
-                op_YXY .+= 8.0, "Sy", j, "Sx", j + l1, "Sy", j + l2
-                op_ZXZ .+= 8.0, "Sz", j, "Sx", j + l1, "Sz", j + l2
-                op_YXZ .+= 8.0, "Sy", j, "Sx", j + l1, "Sz", j + l2
-                op_ZXY .+= 8.0, "Sz", j, "Sx", j + l1, "Sy", j + l2
-
-                op_YYX .+= 8.0, "Sy", j, "Sy", j + l1, "Sx", j + l2
-                op_ZZX .+= 8.0, "Sz", j, "Sz", j + l1, "Sx", j + l2
-                op_YZX .+= 8.0, "Sy", j, "Sz", j + l1, "Sx", j + l2
-                op_ZYX .+= 8.0, "Sz", j, "Sy", j + l1, "Sx", j + l2
-
-                op_XYY .+= 8.0, "Sx", j, "Sy", j + l1, "Sy", j + l2
-                op_XZZ .+= 8.0, "Sx", j, "Sz", j + l1, "Sz", j + l2
-                op_XYZ .+= 8.0, "Sx", j, "Sy", j + l1, "Sz", j + l2
-                op_XZY .+= 8.0, "Sx", j, "Sz", j + l1, "Sy", j + l2
-            end
-            push!(op_list, op_XXX)
-
-            push!(op_list, op_YXY)
-            push!(op_list, op_ZXZ)
-            push!(op_list, op_YXZ)
-            push!(op_list, op_ZXY)
-
-            push!(op_list, op_YYX)
-            push!(op_list, op_ZZX)
-            push!(op_list, op_YZX)
-            push!(op_list, op_ZYX)
-
-            push!(op_list, op_XYY)
-            push!(op_list, op_XZZ)
-            push!(op_list, op_XYZ)
-            push!(op_list, op_XZY)
-        end
+    #bulk terms
+    for j in 1:(N - 2)
+        op_XXX .+= 8.0, "Sx", j, "Sx", j + 1, "Sx", j + 2
+        op_YXY .+= 8.0, "Sy", j, "Sx", j + 1, "Sy", j + 2
+        op_ZXZ .+= 8.0, "Sz", j, "Sx", j + 1, "Sz", j + 2
+        op_YXZ .+= 8.0, "Sy", j, "Sx", j + 1, "Sz", j + 2
+        op_ZXY .+= 8.0, "Sz", j, "Sx", j + 1, "Sy", j + 2
     end
+    #corresponding boundary terms
+    # op_XXX .+= 4.0, "Sx", 1, "Sx", 2
+    # op_XXX .+= 4.0, "Sx", N - 1, "Sx", N
+    # op_YXY .+= 4.0, "Sx", 1, "Sy", 2
+    # op_YXY .+= 4.0, "Sy", N - 1, "Sx", N
+    # op_ZXZ .+= 4.0, "Sx", 1, "Sz", 2
+    # op_ZXZ .+= 4.0, "Sz", N - 1, "Sx", N
+    # op_YXZ .+= 4.0, "Sx", 1, "Sz", 2
+    # op_YXZ .+= 4.0, "Sy", N - 1, "Sx", N
+    # op_ZXY .+= 4.0, "Sx", 1, "Sy", 2
+    # op_ZXY .+= 4.0, "Sz", N - 1, "Sx", N
+
+    push!(op_list, op_XXX)
+    push!(op_list, op_YXY)
+    push!(op_list, op_ZXZ)
+    push!(op_list, op_YXZ)
+    push!(op_list, op_ZXY)
+
+    #boundary terms - single Pauli
+    X1 = AutoMPO(); X1 .+= 2.0, "Sx", 1
+    XN = AutoMPO(); XN .+= 2.0, "Sx", N
+    Y1 = AutoMPO(); Y1 .+= 2.0, "Sy", 1
+    YN = AutoMPO(); YN .+= 2.0, "Sy", N
+    Z1 = AutoMPO(); Z1 .+= 2.0, "Sz", 1
+    ZN = AutoMPO(); ZN .+= 2.0, "Sz", N
+
+    push!(op_list, X1)
+    push!(op_list, XN)
+    push!(op_list, Y1)
+    push!(op_list, YN)
+    push!(op_list, Z1)
+    push!(op_list, ZN)
+
+    #boundary terms - two Paulis
+    XX1 = AutoMPO(); XX1 .+= 4.0, "Sx", 1, "Sx", 2
+    XY1 = AutoMPO(); XY1 .+= 4.0, "Sx", 1, "Sy", 2
+    XZ1 = AutoMPO(); XZ1 .+= 4.0, "Sx", 1, "Sz", 2
+    YX1 = AutoMPO(); YX1 .+= 4.0, "Sy", 1, "Sx", 2
+    YY1 = AutoMPO(); YY1 .+= 4.0, "Sy", 1, "Sy", 2
+    YZ1 = AutoMPO(); YZ1 .+= 4.0, "Sy", 1, "Sz", 2
+    ZX1 = AutoMPO(); ZX1 .+= 4.0, "Sz", 1, "Sx", 2
+    ZY1 = AutoMPO(); ZY1 .+= 4.0, "Sz", 1, "Sy", 2
+    ZZ1 = AutoMPO(); ZZ1 .+= 4.0, "Sz", 1, "Sz", 2
+    push!(op_list, XX1)
+    push!(op_list, XY1)
+    push!(op_list, XZ1)
+    push!(op_list, YX1)
+    push!(op_list, YY1)
+    push!(op_list, YZ1)
+    push!(op_list, ZX1)
+    push!(op_list, ZY1)
+    push!(op_list, ZZ1)
+
+    XXN = AutoMPO(); XXN .+= 4.0, "Sx", N - 1, "Sx", N
+    XYN = AutoMPO(); XYN .+= 4.0, "Sx", N - 1, "Sy", N
+    XZN = AutoMPO(); XZN .+= 4.0, "Sx", N - 1, "Sz", N
+    YXN = AutoMPO(); YXN .+= 4.0, "Sy", N - 1, "Sx", N
+    YYN = AutoMPO(); YYN .+= 4.0, "Sy", N - 1, "Sy", N
+    YZN = AutoMPO(); YZN .+= 4.0, "Sy", N - 1, "Sz", N
+    ZXN = AutoMPO(); ZXN .+= 4.0, "Sz", N - 1, "Sx", N
+    ZYN = AutoMPO(); ZYN .+= 4.0, "Sz", N - 1, "Sy", N
+    ZZN = AutoMPO(); ZZN .+= 4.0, "Sz", N - 1, "Sz", N
+    push!(op_list, XXN)
+    push!(op_list, XYN)
+    push!(op_list, XZN)
+    push!(op_list, YXN)
+    push!(op_list, YYN)
+    push!(op_list, YZN)
+    push!(op_list, ZXN)
+    push!(op_list, ZYN)
+    push!(op_list, ZZN)
+    
     return op_list
 end
 
-function HamiltonianLearner(psi::MPS, range::Int=2,
-    method::Union{Symbol, Nothing}=nothing
-    )
+function HamiltonianLearner(psi::MPS, method::Union{Symbol, Nothing}=nothing)
 
     sites = siteinds(psi)
     N = length(sites)
@@ -134,15 +161,16 @@ function HamiltonianLearner(psi::MPS, range::Int=2,
         method = :corr_mat
     end
 
-    op_list = generate_custom_op_list(sites, range)
+    op_list = generate_custom_op_list(sites)
     coeff_list = zeros(Float64, length(op_list))
 
     G_mat = zeros(Float64, 0, 0)
+    exp_vals = zeros(Float64, 0)
 
     H = MPO()
     ψ_GS = MPS()    
 
-    return HamiltonianLearner(N, sites, copy(psi), coeff_list, op_list, range, method, G_mat, H, ψ_GS, 0.0, 0.0, 0.0, 1.0)
+    return HamiltonianLearner(N, sites, copy(psi), coeff_list, op_list, method, G_mat, exp_vals, H, ψ_GS, 0.0, 0.0, 0.0, 1.0)
 end
 
 include("HL1D_utils.jl")
